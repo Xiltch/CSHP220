6d472d38d1a0d711d71b9eb60d80e7eed98b8fcd
Jonathan Tweedle <xiltch@gmail.com>
2019-04-14 10:45:09 -0700

    second window users

---
+++ HomeworkTwo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HomeworkTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // capture a stream to be used for outputting the results to
+            TextWriter stream = Console.Out;
+
+            // Create a list of users to process
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            // Finds all users with the password "hello" and outputs them to the console
+            users
+                .Where(x => "hello".Equals(x.Password))
+                .ToList()
+                .ForEach(stream.WriteLine);
+
+        }
+    }
+}
