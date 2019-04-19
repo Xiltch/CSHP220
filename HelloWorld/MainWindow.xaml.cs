978e02e932dc52a7299ca95b6cf1ea6728f992da
Jonathan Tweedle <xiltch@gmail.com>
2019-04-18 18:52:28 -0700

    added list connect to the database user entities

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace HelloWorld
 {
@@ -31,7 +32,15 @@ namespace HelloWorld
         public override void EndInit()
         {
             base.EndInit();
+
             uxContainer.DataContext = user;
+
+            var sample = new SampleEntities();
+
+            sample.Users.Load();
+
+            uxList.ItemsSource = sample.Users.Local;
+
         }
 
         private void uxSubmit_Click(object sender, RoutedEventArgs e)
