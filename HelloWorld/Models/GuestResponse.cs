f3674d90845d4619718762822f10fb5f80f14934
Jonathan Tweedle <xiltch@gmail.com>
2019-05-16 18:38:54 -0700

    added bootstrap and some styling to the views

--- HelloWorld/Models/GuestResponse.cs
+++ HelloWorld/Models/GuestResponse.cs
@@ -10,8 +10,11 @@ namespace HelloWorld.Models
     {
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+        [Phone]
         public string Phone { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please select attendance response")]
         public bool? WillAttend { get; set; }
 
     }
