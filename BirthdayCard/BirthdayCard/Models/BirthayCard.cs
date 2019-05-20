08e7f7e0730e4cc04ae007e39f592f359d9517d9
Jonathan <xiltch@gmail.com>
2019-05-20 11:46:10 -0700

    updated validation

--- BirthdayCard/BirthdayCard/Models/BirthayCard.cs
+++ BirthdayCard/BirthdayCard/Models/BirthayCard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,11 @@ namespace BirthdayCard.Models
 {
     public class BirthayCard
     {
-
-        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter who the card is from")]
         public string From { get; set; }
+        [Required(ErrorMessage = "Please enter who the card is to")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter a message for the card")]
         public string Message { get; set; }
 
     }
