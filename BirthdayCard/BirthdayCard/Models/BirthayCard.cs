10617b489041ec045194df2ca4f94db9006f2cc2
Jonathan <xiltch@gmail.com>
2019-05-20 11:24:27 -0700

    modelling data needed for the form

---
+++ BirthdayCard/BirthdayCard/Models/BirthayCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayCard.Models
+{
+    public class BirthayCard
+    {
+
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Message { get; set; }
+
+    }
+}
