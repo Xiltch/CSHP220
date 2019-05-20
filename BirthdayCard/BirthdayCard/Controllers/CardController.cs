81469ef60d8e0e9b20fa703e623552ff9c24b252
Jonathan <xiltch@gmail.com>
2019-05-20 11:31:27 -0700

    new birtday card form

---
+++ BirthdayCard/BirthdayCard/Controllers/CardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Card = BirthdayCard.Models.BirthayCard;
+
+namespace BirthdayCard.Controllers
+{
+    public class CardController : Controller
+    {
+        // GET: Card
+        public ActionResult New()
+        {
+            Card card = new Card();
+            return View(card);
+        }
+
+        
+    }
+}
