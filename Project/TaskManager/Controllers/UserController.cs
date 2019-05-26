2ef4f98e62f37a710db36b04a2ffc69bca0f70ec
Jonathan <xiltch@gmail.com>
2019-05-26 11:46:49 -0700

    adding user controller

---
+++ Project/TaskManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Blueprints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskManager.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly ITaskRepository repository;
+
+        public UserController(ITaskRepository context)
+        {
+            this.repository = context;
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
